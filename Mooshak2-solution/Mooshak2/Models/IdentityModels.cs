5b32a037fc831424e0e68a06d4ea35986c89437d
aevartg <aevartg@gmail.com>
2016-05-02 15:55:51 +0000

    identitymodels

--- Mooshak2-solution/Mooshak2/Models/IdentityModels.cs
+++ Mooshak2-solution/Mooshak2/Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Mooshak2.Models.EntityClasses;
 
 namespace Mooshak2.Models
 {
@@ -20,6 +21,12 @@ namespace Mooshak2.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+	    public DbSet<Course> Courses { get; set; }
+	    public DbSet<Milestone> Milestones { get; set; }
+	    public DbSet<Assignment> Assignments { get; set; }
+	    public DbSet<InputOutput> InputOutputs { get; set; }
+	    public DbSet<Submission> Submissions { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
