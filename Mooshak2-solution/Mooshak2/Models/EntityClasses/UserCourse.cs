01a8c5a5c127180ab280040f7730a7a4c66a8512
Eyþór A <senpai@plznotice.me>
2016-05-04 23:04:32 +0000

    added UserCourse entity for link table

---
+++ Mooshak2-solution/Mooshak2/Models/EntityClasses/UserCourse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2.Models.EntityClasses
+{
+	public class UserCourse
+	{
+		[Key,ForeignKey("User"),Column(Order = 1)]
+		public string UserId { get; set; }
+		[Key, ForeignKey("Course"), Column(Order = 0)]
+		public int CourseId { get; set; }
+
+		public virtual ApplicationUser User { get; set; }
+		public virtual Course Course { get; set; }
+	}
+}
