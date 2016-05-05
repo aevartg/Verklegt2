5f51317336f4fdeee658e47b27d9a1f36fa8fa90
Eyþór A <senpai@plznotice.me>
2016-05-05 21:12:24 +0000

    added so that user gains role if ticked

--- Mooshak2-solution/Mooshak2/Controllers/AccountController.cs
+++ Mooshak2-solution/Mooshak2/Controllers/AccountController.cs
@@ -155,6 +155,16 @@ namespace Mooshak2.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+	                if (model.UserType)
+	                {
+		                var manager = new IdentityManager();
+
+						if (!manager.RoleExists("Teacher"))
+						{
+							manager.CreateRole("Teacher");
+						}
+		                manager.AddUserToRole(user.Id,"Teacher");
+	                }
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
