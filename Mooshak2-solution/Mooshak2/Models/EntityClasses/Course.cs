7a53fc735ee7ed8e57736c2ed2fc9b66ab2a13dd
Eyþór A <senpai@plznotice.me>
2016-05-06 16:43:23 +0000

    Code first database rebuild

--- Mooshak2-solution/Mooshak2/Models/EntityClasses/Course.cs
+++ Mooshak2-solution/Mooshak2/Models/EntityClasses/Course.cs
@@ -1,8 +1,14 @@
-﻿namespace Mooshak2.Models.EntityClasses
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mooshak2.Models.EntityClasses
 {
 	public class Course
 	{
+		[Key]
 		public int Id { get; set; }
+		[Required]
 		public string Name { get; set; }
+		public ICollection<ApplicationUser> Users { get; set; }
 	}
 }
--- Mooshak2-solution/Mooshak2/Models/EntityClasses/InputOutput.cs
+++ Mooshak2-solution/Mooshak2/Models/EntityClasses/InputOutput.cs
@@ -1,10 +1,17 @@
-﻿namespace Mooshak2.Models.EntityClasses
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mooshak2.Models.EntityClasses
 {
 	public class InputOutput
 	{
+		[Key]
 		public int Id { get; set; }
+		[ForeignKey("Milestone")]
 		public int MilestoneId { get; set; }
 		public string Input { get; set; }
+		[Required]
 		public string Output { get; set; }
+		public virtual Milestone Milestone { get; set; }
 	}
 }
