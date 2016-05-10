d3a1f65587db0aa4b48d897141df184fbae47c46
Eyþór A <senpai@plznotice.me>
2016-05-10 11:11:35 +0000

    database connections

---
+++ Mooshak2-solution/Mooshak2/Services/InputOutputService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mooshak2.Models;
+using Mooshak2.Models.EntityClasses;
+
+namespace Mooshak2.Services
+{
+	public class InputOutputService
+	{
+		private ApplicationDbContext _db;
+		public InputOutputService()
+		{
+			_db = new ApplicationDbContext();
+		}
+
+		public bool CreateInputOutput(string input, string output, int milestoneId)
+		{
+			var milestone = (from x in _db.Milestones where x.Id == milestoneId select x).Single();
+			var temp = new InputOutput()
+						{
+							Input = input,
+							Output = output,
+							Milestone = milestone,
+							MilestoneId = milestone.Id
+						};
+			_db.InputOutputs.Add(temp);
+			return (_db.SaveChanges() > 0);
+		}
+	}
+}
