4c1bb79cdd30dd4e72971d488175d28601daab36
Eyþór A <senpai@plznotice.me>
2016-05-11 04:44:06 +0000

    need to clean up code abit

---
+++ Mooshak2-solution/Mooshak2/Models/EntityClasses/UserOutput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2.Models.EntityClasses
+{
+	public class UserOutput
+	{
+		[Key]
+		public int Id { get; set; }
+		[ForeignKey("InputOutput")]
+		public int InputId { get; set; }
+		[ForeignKey("ApplicationUser")]
+		public String UserId { get; set; }
+		[Required]
+		public string Output { get; set; }
+
+		public virtual InputOutput InputOutput { get; set; }
+		public virtual ApplicationUser ApplicationUser { get; set; }
+	}
+}
