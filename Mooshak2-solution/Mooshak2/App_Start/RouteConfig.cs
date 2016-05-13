97cc8fe65e90443a93784860b6931cd9e2b2951c
Eyþór A <senpai@plznotice.me>
2016-05-13 00:19:44 +0000

    the start of the exception handling is here

--- Mooshak2-solution/Mooshak2/App_Start/RouteConfig.cs
+++ Mooshak2-solution/Mooshak2/App_Start/RouteConfig.cs
@@ -14,6 +14,17 @@ namespace Mooshak2
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
 			routes.MapRoute(
+				name: "FailFish",
+				url: "FailFish/{action}/{id}",
+				defaults: new
+						{
+							controller = "Error",
+							action = "FailFish",
+							id = UrlParameter.Optional
+						}
+			);
+
+			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
 				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
