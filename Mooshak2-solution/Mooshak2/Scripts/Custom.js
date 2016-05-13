65d3a01186765d1a0b97fe57acabb584b6fc2f74
aevartg <aevar@DESKTOP-1LUUTER>
2016-05-13 14:29:16 +0000

    lagaði navbar í teacher

--- Mooshak2-solution/Mooshak2/Scripts/Custom.js
+++ Mooshak2-solution/Mooshak2/Scripts/Custom.js
@@ -158,25 +158,6 @@ $(document).ready(function () {
 	})
 })
 
-$(document).ready(function () {
-
-	$(".table tbody tr").each(function () {
-		$(this).attr("searchitem", $(this).find(".username").text().toLowerCase());
-	});
-
-	$(".leitarbox").on("keyup", function () {
-		var searchTerm = $(this).val().toLowerCase();
-
-		$(".table tbody tr").each(function () {
-			if ($(this).filter("[searchitem *= " + searchTerm + "]").length > 0 || searchTerm.length < 1) {
-				$(this).show();
-			} else {
-				$(this).hide();
-			}
-		});
-	});
-});
-
 //$(window).load(function () {
 //	 /*Animate loader off screen*/
 //	$(".se-pre-con").fadeOut("slow");;
